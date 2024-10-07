3343401a599ba628c7e9d46b0c4567533cfad43d
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-10-07 13:26:16 +0200

    hashing with that CLI

---
+++ konichiwa.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+
+string x = "konichiwa \n";
+string z = "bye-bye \n";
+
+printf("%s %s \n", x, z);
+
+return 0;
+}
+
+// another way 
+int main(void){
+
+string greetings[3];
+
+string greetings[1] = "hello \n";
+string greetings[2] = "konichiwa \n";
+string greetings[3] = "dames en here \n";
+
+
+printf("%s %s %s \n", greetings[]  );
+
+return 0;
+}

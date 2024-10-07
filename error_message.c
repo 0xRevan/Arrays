bad6a176ad7bdb77677eabcf6e57905135f5557e
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-10-07 13:59:23 +0200

    able to generate an error message if incorrect input is added

---
+++ error_message.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[]){
+
+    if (argc != 2)
+    {
+        printf("Error, bruv. Check your code \n");
+        return 999;
+    }
+    
+    else{
+        printf("hello, %s" ", your input has been accepted \n", argv[1]);
+    }
+}
